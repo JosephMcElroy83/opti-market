e3ae2c97a59544559ba8f43590223c6a93c3fbc5
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-11-04 15:14:28 -0400

    updating database Products to include image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_143904) do
+ActiveRecord::Schema.define(version: 2021_11_04_190847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_143904) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.bigint "category_id", null: false
+    t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ puts "#{User.count} users created"
 
 @category1 = Category.create!(title: 'electronics')
 
-@product1 = Product.create!(title: 'Mac Book Pro 13', description: 'some description here, bla bla bla', price: '750', isFeatured: false, saleValue: 0, category: @category1,user: @admin)
+@product1 = Product.create!(title: 'Mac Book Pro 13', description: 'some description here, bla bla bla', price: '750', image: 'https://cdn.pixabay.com/photo/2015/01/14/18/41/home-office-599475_1280.jpg' , isFeatured: false, saleValue: 0, category: @category1,user: @admin)
