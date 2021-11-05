a67b870d6d89a7dbe7810498998552c9f5ca170a
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-11-05 16:39:58 -0400

    fixed products axios requests to fit backend changes

--- client/src/Services/products.js
+++ client/src/Services/products.js
@@ -1,26 +1,26 @@
 import api from './api-config';
 
-export const getAllProducts = async () => {
-  const resp = await api.get('/products');
-  return resp.data;
-};
+export const createProduct = async (data) => {
+  const resp = await api.post('/products', { product: data })
+  return resp.data
+}
 
-export const getOneProducts = async (id) => {
+export const readAllProducts = async () => {
+  const resp = await api.get('/products')
+  return resp.data
+}
+
+export const readOneProduct = async (id) => {
   const resp = await api.get(`/products/${id}`);
-  return resp.data;
-};
+  return resp.data
+}
 
-export const postProduct = async (productData) => {
-  const resp = await api.post('/products', { food: productData });
-  return resp.data;
-};
+export const updateProduct = async (id, data) => {
+  const resp = await api.put(`/products/${id}`, { product: data })
+  return resp.data
+}
 
-export const putProduct = async (id, productData) => {
-  const resp = await api.put(`/products/${id}`, { food: productData });
-  return resp.data;
-};
-
-export const deleteProduct = async (id) => {
-  const resp = await api.delete(`/products/${id}`);
-  return resp;
-};
+export const destroyProduct = async (id) => {
+  const resp = await api.delete(`/products/${id}`)
+  return resp.data
+}
