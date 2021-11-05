a241347431eadeda6948d935f53ad09b2e2dad29
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-11-05 16:38:27 -0400

    Create working for products with form

--- client/src/Screens/ProductCreate.jsx
+++ client/src/Screens/ProductCreate.jsx
@@ -2,31 +2,101 @@ import { useState } from 'react';
 
 export default function ProductCreate(props) {
   const [formData, setFormData] = useState({
-    name: '',
+    title: "",
+    description: "",
+    category_id: 1,
+    price: "",
+    isFeatured: false,
+    saleValue: 0,
+    image: "",
   });
-  const { name } = formData;
-  const { handleProductCreate } = props;
+  const { title, description, price, image, category_id, isFeatured, saleValue } = formData;
+  const { newProduct } = props;
 
   const handleChange = (e) => {
-    const { value } = e.target;
-    setFormData({
-      name: value,
-    });
+    const { name, value } = e.target;
+    setFormData(prevForm => ({
+      ...prevForm,
+      [name]: value
+    }));
   };
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        handleProductCreate(formData);
+        newProduct(formData);
       }}
     >
       <h1>Create Product</h1>
       <label>
-        Name:
-        <input type='text' value={name} onChange={handleChange} />
-      </label>
-      <br />
+          Photo Link:
+            <input
+            type="text"
+            name="image"
+            value={image}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Product's Title:
+            <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Product's Description:
+            <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Product's Category:
+            <input
+            type="text"
+            name="category_id"
+            value={category_id}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Product's Price:
+            <input
+            type="text"
+            name="price"
+            value={price}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Product Featured:
+            <input
+            type="text"
+            name="isFeatured"
+            value={isFeatured}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Product's Sale Percent:
+            <input
+            type="text"
+            name="saleValue"
+            value={saleValue}
+            onChange={handleChange}
+          />
+        </label>
       <button>Submit</button>
     </form>
   );
