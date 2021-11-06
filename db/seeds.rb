f252293ccd6a4bc3121e1c9d30d532992c9b809c
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-11-06 17:06:20 -0400

    added new testing seeed data to account for updated category listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-@admin = User.create!(username: 'joe', email: 'joe@email.com', password: '123456')
+Product.destroy_all
+Category.destroy_all
+User.destroy_all
+
+@admin = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
 
 puts "#{User.count} users created"
 
 @category1 = Category.create!(title: 'electronics')
+@category2 = Category.create!(title: 'appliances')
+@category3 = Category.create!(title: 'apparel')
 
-@product1 = Product.create!(title: 'Mac Book Pro 13', description: 'some description here, bla bla bla', price: '750', image: 'https://cdn.pixabay.com/photo/2015/01/14/18/41/home-office-599475_1280.jpg' , isFeatured: false, saleValue: 0, category: @category1,user: @admin)
+puts "#{Category.count} categories created"
+
+@test1 = Product.create!(title: 'kitchen set', description: 'some description here, bla bla bla', price: '750', image: 'https://cdn.pixabay.com/photo/2015/01/14/18/41/home-office-599475_1280.jpg' , isFeatured: false, saleValue: 0, category: @category2,user: @admin)
+@test2 = Product.create!(title: 'men\'s blazer', description: 'some description here, bla bla bla', price: '750', image: 'https://cdn.pixabay.com/photo/2015/01/14/18/41/home-office-599475_1280.jpg' , isFeatured: false, saleValue: 0, category: @category3,user: @admin)
+@test3 = Product.create!(title: 'Mac Book Pro 13', description: 'some description here, bla bla bla', price: '750', image: 'https://cdn.pixabay.com/photo/2015/01/14/18/41/home-office-599475_1280.jpg' , isFeatured: false, saleValue: 0, category: @category1,user: @admin)
+
+puts "#{Product.count} products created"
